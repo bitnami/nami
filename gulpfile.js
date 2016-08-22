5a16ede0a95a5c866f41e58b4a293b49eff50cff
jfuentes <jfuentes@bitnami.com>
2016-08-22 23:17:58 +0200

    changes in postBundleFilter

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,10 @@
 'use strict';
 
+const _ = require('lodash');
 const gulp = require('gulp');
 const commonTasks = require('bitnami-gulp-common-tasks')(gulp);
 const runSequence = require('run-sequence');
 
-const runtimeVersion = '6.2.1';
-const runtimeDir = './runtime';
 
 /* CI tasks */
 
@@ -24,8 +23,8 @@ const runtime = {url: process.env.NODE_RUNTIME_URL || null};
 
 if (!runtime.url) {
   runtime.name = 'node';
-  runtime.version = runtimeVersion;
-  runtime.destDir = runtimeDir;
+  runtime.version = '6.2.1';
+  runtime.destDir = './runtime';
 }
 
 const npmRegistry = process.env.NPM_REGISTRY || null;
@@ -39,6 +38,37 @@ const bundledPkgs = {
   'nami-test': defaultFilter,
   'cmd-parser': defaultFilter
 };
+
+// Files to be stripped out in the final distributable
+const postBundleFilter = [
+  '**/flycheck_*.js',
+  '**/*~',
+  '**/*.patch',
+  '**/.git'
+].concat(_.map(['tests', 'test', 'tests.js', 'test.js'], (p) => `node_modules/**/${p}`))
+.concat(_.map(['tests', 'test', 'tests.js', 'test.js'], (p) => `!node_modules/**/mocha/**/${p}`))
+.concat(_.map(['tests', 'test', 'tests.js', 'test.js'], (p) => `!node_modules/**/chai/**/${p}`))
+.concat(_.map(['code_of_conduct.md', 'releasenotes.md', 'readme.md', 'history.md',
+               'changelog.md', 'contributing.md', 'license.md', 'changes.md'], (p) => `**/${p}`))
+.concat(_.map(['nomnom', 'jsdoc-toolkit', 'nan'], (p) => `node_modules/${p}`))
+.concat(['node_modules/deasync/bin/*', '!node_modules/deasync/bin/linux-x64-node-6/**'])
+.concat(['node_modules/JSV'])
+.concat([
+	 'node_modules/core-js/*.js',
+         'node_modules/core-js/LICENSE',
+	 '!node_modules/core-js/package.json',
+         '!node_modules/core-js/index.js', 
+         'node_modules/core-js/es*',
+         'node_modules/core-js/client',
+         'node_modules/core-js/core',
+         'node_modules/core-js/fn',
+         'node_modules/core-js/build',
+         'node_modules/core-js/js',
+         'node_modules/core-js/modules',
+         'node_modules/core-js/web',
+	]);
+
+
 
 commonTasks.bundle({
   buildDir,
@@ -54,6 +84,7 @@ commonTasks.bundle({
     'index.js',
     'cli{,/**}',
   ],
+  postBundleFilter,
   bundledPkgs,
   entrypoint: 'index.js',
   npmRegistry,
@@ -75,8 +106,6 @@ commonTasks.npm({
 });
 
 /* General tasks */
-
-commonTasks['install-node']({version: runtimeVersion, destination: runtimeDir});
 
 gulp.task('clean', () => {
   runSequence('test:clean', 'ci-test:clean', 'bundle:clean');
